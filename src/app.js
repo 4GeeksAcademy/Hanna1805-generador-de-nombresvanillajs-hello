1fd588f4679773b782c99d4ca575f5eba336108e
Hanna1805 <dreamy803718@gmail.com>
2026-01-14 15:09:27 +0000

    Crear archivo Hanna

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const generadorDominio = () => {
+    let pronoun = ['the', 'our'];
+    let adj = ['great', 'big'];
+    let noun = ['jogger', 'racoon'];
+    let dom =[".com", ".es"];
+
+   for (let i = 0; i < pronoun.length; i++) {
+     const pronombre = pronoun[i];
+    
+      for (let ia = 0; ia < adj.length; ia++) {
+        const adjetivo = adj[ia];
+
+        for (let inon = 0; inon < noun.length; inon++) {
+         const sustantivo = noun[inon];
+ 
+         for (let id = 0; id < dom.length; id++){
+           const dominio = dom[id];
+
+           let combination = pronombre + adjetivo + sustantivo + dominio;
+            console.log(combination)
+          }  
+        }
+      }
+    }
+  }
+ generadorDominio();
+}
